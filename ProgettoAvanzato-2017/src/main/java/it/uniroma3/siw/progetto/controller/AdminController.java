eecac574156b95ab534f1d78abda2a84a6912f46
Dorian Shurdhi <dorianshurdhi@hotmail.it>
2017-06-14 16:12:13 +0200

    add Controller class

--- ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/controller/AdminController.java
+++ ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/controller/AdminController.java
@@ -1,5 +1,83 @@
 package it.uniroma3.siw.progetto.controller;
 
-public class AdminController {
+import java.util.LinkedList;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import it.uniroma3.siw.progetto.model.Admin;
+import it.uniroma3.siw.progetto.model.Autore;
+import it.uniroma3.siw.progetto.model.Opera;
+import it.uniroma3.siw.progetto.service.AdminService;
+import it.uniroma3.siw.progetto.service.AutoreService;
+import it.uniroma3.siw.progetto.service.OperaService;
+
+@Controller
+public class AdminController {
+	@Autowired
+	AdminService adminService;
+	@Autowired
+	OperaService operaService;
+	@Autowired
+	AutoreService autoreService;
+
+	@PostMapping("/adminLogin")
+	public String checkUtenteInfo(@Valid @ModelAttribute Admin admin, BindingResult bindingResult, Model model) {
+
+		if (bindingResult.hasErrors()) {
+			return "LoginAdmin"; //in caso di errore 
+		} else {
+			if(adminService.findByUserName(admin.getUsername())==null) {
+				return "LoginAdmin";
+			} else {
+				model.addAttribute("nomeAdmin",admin.getUsername());
+			}
+		}
+		return "OperazioniAdmin";
+	}
+	
+	@PostMapping("/adminReg")
+	public String insertNewAdmin(@Valid @ModelAttribute Admin admin, BindingResult bindingResult, Model model) {
+
+		if (bindingResult.hasErrors()) {
+			return "RegistrazioneAdmin"; //in caso di errore 
+		} else {
+			model.addAttribute(admin);
+			adminService.add(admin); 
+		}
+
+		return "OperazioniAdmin";
+	}
+	
+	@PostMapping("/reload")
+	public String reload(@Valid @ModelAttribute Admin admin, BindingResult bindingResult, Model model) {
+		return "OperazioniAdmin";
+	}
+	
+	@ModelAttribute("opere")
+	public Iterable<Opera> opere(){
+		Iterable <Opera> itopere=  operaService.findAll();
+		List<Opera> opere = new LinkedList<>();
+		for(Opera o : itopere){
+			opere.add(o);
+		}
+		return opere;
+	}
+	
+	@ModelAttribute("autori")
+	public Iterable<Autore> operi(){
+		Iterable <Autore> itautori=  autoreService.findAll();
+		List<Autore> autori = new LinkedList<>();
+		for(Autore a : itautori){
+			autori.add(a);
+		}
+		return autori;
+	}
 }
--- ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/controller/OperaController.java
+++ ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/controller/OperaController.java
@@ -1,5 +1,96 @@
 package it.uniroma3.siw.progetto.controller;
 
-public class OperaController {
+import java.util.LinkedList;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import it.uniroma3.siw.modello.Opera;
+import it.uniroma3.siw.service.OperaService;
+
+@Controller
+public class OperaController {
+	
+	@Autowired
+	OperaService operaService;
+	
+    @GetMapping("/opera")
+    public String showForm(Opera opera) {
+        return "form";
+    }
+
+    @PostMapping("/addOpera")
+    public String checkOperaAdd(@Valid @ModelAttribute Opera opera, BindingResult bindingResult, Model model) {
+    	
+        if (bindingResult.hasErrors()) {
+            return "OperazioniAdmin";
+        } else {
+        	operaService.add(opera); 
+        	Iterable <Opera> itopere = operaService.findAll();
+    		List<Opera> opere = new LinkedList<>();
+    		for(Opera o : itopere){
+    			opere.add(o);
+    		}
+    		model.addAttribute("opere", opere);
+        }
+        return "OperazioniAdmin";
+    }
+    
+    @PostMapping("/updateOpera")
+    public String updateOpera(@Valid @ModelAttribute Opera opera, BindingResult bindingResult, Model model) {
+    	
+        if (bindingResult.hasErrors()) {
+            return "OperazioniAdmin";
+        } else {
+        	//TODO SISTEMARE METODO 
+        	operaService.findbyId(opera.getId()); 
+        	Iterable <Opera> itopere = operaService.findAll();
+    		List<Opera> opere = new LinkedList<>();
+    		for(Opera o : itopere){
+    			opere.remove(o);
+    			if(o.getRestauro().equals("Si"))
+    				o.setRestauro("No");
+    			else
+    				o.setRestauro("Si");
+    			opere.add(o);
+    		}
+    		model.addAttribute("opere", opere);
+        }
+        return "OperazioniAdmin";
+    }
+    
+    @PostMapping("/removeOpera")
+    public String operaRemove(@Valid @ModelAttribute Opera opera, BindingResult bindingResult, Model model) {
+    	
+        if (bindingResult.hasErrors()) {
+            return "OperazioniAdmin";
+        } else {
+        	operaService.remove(opera);
+        	Iterable <Opera> itopere = operaService.findAll();
+    		List<Opera> opere = new LinkedList<>();
+    		for(Opera o : itopere){
+    			opere.add(o);
+    		}
+    		model.addAttribute("opere", opere);
+        }
+        return "OperazioniAdmin";
+    }
+    
+	@ModelAttribute("opere")
+	public Iterable<Opera> opere(){
+		Iterable <Opera> itopere = operaService.findAll();
+		List<Opera> opere = new LinkedList<>();
+		for(Opera o : itopere){
+			opere.add(o);
+		}
+		return opere;
+	}
 }
