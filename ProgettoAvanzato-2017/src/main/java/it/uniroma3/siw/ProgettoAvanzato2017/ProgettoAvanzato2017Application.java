c459ddff5704e4526338631af2a381301aea38f4
Dorian Shurdhi <dorianshurdhi@hotmail.it>
2017-06-14 15:27:11 +0200

    progetto avanzato

---
+++ ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/ProgettoAvanzato2017/ProgettoAvanzato2017Application.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw.ProgettoAvanzato2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoAvanzato2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoAvanzato2017Application.class, args);
+	}
+}
