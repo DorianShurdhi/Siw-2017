b1a01f97cde63e5ec32076040e1fe9797b6d811a
Bujar Subashi <subashibujar@hotmail.it>
2017-06-14 16:30:28 +0200

    add Service classes

---
+++ ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/service/AutoreService.java
@@ -0,0 +1,42 @@
+package it.uniroma3.siw.progetto.service;
+
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.siw.progetto.model.Autore;
+import it.uniroma3.siw.progetto.repository.AutoreRepository;
+
+@Service
+public class AutoreService {
+	
+	@Autowired
+	private AutoreRepository autoreRepository;
+	
+    public Iterable<Autore> findAll() {
+        return this.autoreRepository.findAll();
+    }
+    
+    @Transactional
+    public void add(final Autore autore) {
+        this.autoreRepository.save(autore);
+    }
+
+	public Iterable<Autore> findByCognome(String cognome) {
+		return this.autoreRepository.findByNazionalita(cognome);
+	}
+    
+	public Iterable<Autore> findByNazionalita(String nazionalita) {
+		return this.autoreRepository.findByNazionalita(nazionalita);
+	}
+	
+	public Iterable<Autore> findByDataNascita(Date dataNascita){
+		return this.autoreRepository.findByDataNascita(dataNascita);
+	}
+	
+	public Iterable<Autore> findByDataMorte(Date dataMorte){
+		return this.autoreRepository.findByDataMorte(dataMorte);
+	}
+	
+}
