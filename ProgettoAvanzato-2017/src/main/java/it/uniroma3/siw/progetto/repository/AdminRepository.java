c43b3f4c8e826e9770e4c4d43abd5064a09ab4ff
Dorian Shurdhi <dorianshurdhi@hotmail.it>
2017-06-15 16:04:40 +0200

    ..

--- ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/repository/AdminRepository.java
+++ ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/repository/AdminRepository.java
@@ -3,7 +3,7 @@ package it.uniroma3.siw.progetto.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.progetto.model.Admin;
-
+//
 public interface AdminRepository  extends CrudRepository<Admin, Long>{
 	Admin findByUsername(String username);
 	
