fdcd8b4575c2c7d5b412067dc8f0d6669d9fde71
Bujar Subashi <subashibujar@hotmail.it>
2017-06-15 19:51:06 +0200

    piccola modifica

--- ProgettoAvanzato-2017x/src/main/java/it/uniroma3/siw/ProgettoAvanzato2017x/service/OperaService.java
+++ ProgettoAvanzato-2017x/src/main/java/it/uniroma3/siw/ProgettoAvanzato2017x/service/OperaService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.uniroma3.siw.ProgettoAvanzato2017x.model.Autore;
-import it.uniroma3.siw.ProgettoAvanzato2017x.Opera;
+import it.uniroma3.siw.ProgettoAvanzato2017x.model.Opera;
 import it.uniroma3.siw.ProgettoAvanzato2017x.repository.OperaRepository;
 
 @Service
