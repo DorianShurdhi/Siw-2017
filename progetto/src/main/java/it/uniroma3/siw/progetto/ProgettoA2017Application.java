b08e209b7fc2be9e61f739717aa94ead3e9d3c62
Bujar Subashi <subashibujar@hotmail.it>
2017-06-15 19:00:19 +0200

    ...

---
+++ progetto/src/main/java/it/uniroma3/siw/progetto/ProgettoA2017Application.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw.progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoA2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoA2017Application.class, args);
+	}
+}
---
+++ progetto/src/main/java/it/uniroma3/siw/progetto/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.progetto;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProgettoA2017Application.class);
+	}
+
+}
