4e1516a7bd2cf3d3cb887952cfb7975518a6b506
Dorian Shurdhi <dorianshurdhi@hotmail.it>
2017-06-16 02:31:39 +0200

    ...

--- ProgettoAvanzato-2017x/src/main/java/it/uniroma3/siw/ProgettoAvanzato2017x/controller/AdminController.java
+++ ProgettoAvanzato-2017x/src/main/java/it/uniroma3/siw/ProgettoAvanzato2017x/controller/AdminController.java
@@ -22,7 +22,7 @@ import it.uniroma3.siw.ProgettoAvanzato2017x.service.OperaService;
 @Controller
 public class AdminController {
 	@Autowired
-	AdminService adminService;   //questo e un commento
+	AdminService adminService;
 	@Autowired
 	OperaService operaService;
 	@Autowired
--- ProgettoAvanzato-2017x/src/main/java/it/uniroma3/siw/ProgettoAvanzato2017x/repository/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw.ProgettoAvanzato2017x.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.siw.ProgettoAvanzato2017x.model.Utente;
-
-public interface UtenteRepository extends CrudRepository<Utente, Long> {
-	  Utente findByUsername(String username);
-}
