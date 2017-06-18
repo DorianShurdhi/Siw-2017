eb999b2fe9d7d24275335c0992a4694b1745bd90
Dorian Shurdhi <dorianshurdhi@hotmail.it>
2017-06-18 02:19:48 +0200

    ....

--- ProgettoAvanzato-2017/src/main/java/it/uniroma3/siw/progetto/repository/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw.progetto.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.siw.progetto.model.Utente;
-
-public interface UtenteRepository extends CrudRepository<Utente, Long> {
-	  Utente findByUsername(String username);
-}
